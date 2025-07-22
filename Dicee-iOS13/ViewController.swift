501f7835c629ee185a0fb1352af2fed1389582de
Alex Brito <alexbrito@Alexs-MacBook-Pro.local>
2025-07-21 21:37:27 -0400

    Finished

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    let diceImages: [UIImage] = [
+        UIImage(imageLiteralResourceName: "DiceOne"),
+        UIImage(imageLiteralResourceName: "DiceTwo"),
+        UIImage(imageLiteralResourceName: "DiceThree"),
+        UIImage(imageLiteralResourceName: "DiceFour"),
+        UIImage(imageLiteralResourceName: "DiceFive"),
+        UIImage(imageLiteralResourceName: "DiceSix")]
+    
+    var isPlayerOneTurn: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        diceImageView1.image = diceImages[rollDice()]
+        diceImageView2.image = diceImages[rollDice()]
     }
 
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        if(isPlayerOneTurn){
+            diceImageView1.image = diceImages[rollDice()]
+        }
+        
+        else {
+            diceImageView2.image = diceImages[rollDice()]
+        }
+        
+        isPlayerOneTurn.toggle()
+        
+    }
+    
+    func rollDice() -> Int {
+        return Int.random(in: 0...5)
+    }
 }
 
